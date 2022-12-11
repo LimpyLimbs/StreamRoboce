c6ed3f7c50a3a46cad1f95d4f51ad5b010a92c9c
eddie <eddie.eby@gmail.com>
2022-12-11 12:48:43 -0700

    changed script

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,7 @@ WORKDIR /home
 COPY . .
 
 RUN apt update -y
-RUN apt install -y vim
 RUN apt install -y /home/google-chrome-stable_current_amd64.deb
 RUN pip install --no-cache-dir -r requirements.txt
 
-CMD [ "sleep", "10000" ]
+CMD [ "python3", "main.py" ]
--- main.py
+++ main.py
@@ -5,24 +5,18 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox") # linux only
-# PATH = r"C:\Users\Eddie\Documents\chromedriver.exe"
-URL = "https://www.twitch.tv/limpylimbs"
+chrome_options.add_argument("--no-sandbox")
+URL = "https://www.twitch.tv/pest"
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get(URL)
 
-quit = False
+exit_script = False
 
-while not quit:
-     None
+while not exit_script:
+    None
 
 '''
 Notes:
-learn how to create a Dockerfile in windows
-learn how to do headless browsing
-import chrome, webdriver,
-Build the image
-__version__ = "4.7.2"
-python is a debian-based OS
+learn how to orchestrate containers
 '''
